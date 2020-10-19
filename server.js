2c2e0f51219ed2ff6b2f49f6a605570e42182227
Kesandu <ceap14@gmail.com>
2020-10-19 03:46:12 +0100

    getting routes almost ready

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+//Route files
+const madamesauce = require('./routes/madame_sauce');
+
 //load env vars
 dotenv.config({ path: './config.env' });
 
 const app = express();
+
+//Mount routers
+app.use('/api/madamesauce', madamesauce);
 
 const PORT = process.env.PORT || 5000;
 
