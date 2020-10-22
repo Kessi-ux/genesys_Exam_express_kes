78ee11c41ccb438ec05d4134b20995d16dde6b4a
Kesandu <ceap14@gmail.com>
2020-10-22 03:48:01 +0100

    colors in console

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
 const connectDB = require('./config/db')
 
 //load env vars
@@ -24,7 +25,14 @@ app.use('/api/menu', menu);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
     PORT,
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-); 
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.bold.yellow)
+);
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    //Close server & exit process
+    server.close(() => process.exit(1));
+});
