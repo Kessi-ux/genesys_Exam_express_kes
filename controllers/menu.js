a60a77ea00a63022e42960e2c0c6d26cf20e0c00
Kesandu <ceap14@gmail.com>
2020-10-29 16:42:35 +0100

    crud function update

--- controllers/menu.js
+++ controllers/menu.js
@@ -7,23 +7,30 @@ const Menu = require('../models/Menu');
 exports.getMenus = async (req, res, next) => {
     try {
         const menus = await Menu.find();
-        res.status(200).json({ success: true, data: menus });
+        res.status(200).json({ success: true, count: menus.length, data: menus });
 
     } catch (err) {
         res.status(400).json({ success: false });
     }
-
-    res.status(200).json({ success: true, msg: 'show all menu' });
 };
 
 //@desc Get single menu
 //@route Get /api/:id
 //@access Public
 
-exports.getMenu = (req, res, next) => {
-    res.status(200).json({ success: true, msq: `show menu ${req.params.id}` });
-};
+exports.getMenu = async (req, res, next) => {
+    try {
+        const menu = await Menu.findbyId(req.params.id);
+        res.status(200).json({ success: true, data: menu });
 
+        if (!menu) {
+            return res.status(400).json({ success: false })
+        }
+
+    } catch (err) {
+        res.status(400).json({ success: false });
+    }
+};
 //@desc create new menus
 //@route Put /api/menu
 //@access Private
@@ -46,14 +53,39 @@ exports.createMenu = async (req, res, next) => {
 //@route Put /api/menu/:id
 //@access Private
 
-exports.updateMenu = (req, res, next) => {
-    res.status(200).json({ success: true, msq: `update menu ${req.params.id}` });
+exports.updateMenu = async (req, res, next) => {
+    try {
+        const menu = await Menu.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+
+        if (!bootcamp) {
+            return res.status(400).json({ success: false });
+        }
+
+        res.status(200).json({ success: true, data: menu });
+
+    } catch (err) {
+        res.status(200).json({ success: false });
+    }
 };
 
-//@desc create new bootcamps
+//@desc create new menu
 //@route delete /api/menu/:id
 //@access Private
 
-exports.deleteMenu = (req, res, next) => {
-    res.status(200).json({ success: true, msq: `delete menu ${req.params.id}` });
+exports.deleteMenu = async (req, res, next) => {
+    try {
+        const menu = await Menu.findByIdAndDelete(req.params.id);
+
+        if (!bootcamp) {
+            return res.status(400).json({ success: false });
+        }
+
+        res.status(200).json({ success: true, data: {} });
+
+    } catch (err) {
+        res.status(200).json({ success: false });
+    }
 };
